a7d06640b598c053578c7f26c7f2de275ba3d28c
lishufei <lishufeier@163.com>
2024-10-29 21:45:42 +0800

    hello初始化

---
+++ src/main/java/com/lsf/hello/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.lsf.hello.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(@RequestParam String name){
+        return name;
+    }
+}
